16d8f2951414b316a83afb99c9d7be9ee132bcd9
PuteriR <puteririzal6@gmail.com>
2023-06-22 12:20:38 +0800

    mencuba

--- ApplicationLayer/managePayment/paymentSuccessful.php
+++ ApplicationLayer/managePayment/paymentSuccessful.php
@@ -14,7 +14,7 @@ $notification1->updateCart();
 ?>
 <html>
     <head>
-        <title>Payment Success</title>
+        <title>Payment Successful</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" type="text/css" href="ExternalCSS/topnav.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
